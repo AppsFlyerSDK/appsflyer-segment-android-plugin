d9f48f80a41f24245ad81460d9919e36d192f0e9
shachar-af <shachar@appsflyer.com>
2016-09-12 15:27:50 +0300

    adding sample app for segment's AppsFlyer integration

--- app/src/main/java/com/segment/analytics/android/integrations/appsflyer/AppsflyerIntegration.java
+++ app/src/main/java/com/segment/analytics/android/integrations/appsflyer/AppsflyerIntegration.java
@@ -1,20 +1,20 @@
 package com.segment.analytics.android.integrations.appsflyer;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
 
 import com.appsflyer.AppsFlyerConversionListener;
 import com.appsflyer.AppsFlyerLib;
-import com.appsflyer.AppsFlyerProperties;
 import com.segment.analytics.Analytics;
 import com.segment.analytics.Properties;
-import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.integrations.IdentifyPayload;
 import com.segment.analytics.integrations.Integration;
 import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.integrations.TrackPayload;
+
 import java.util.Map;
 
 /**
@@ -23,15 +23,14 @@ import java.util.Map;
 public class AppsflyerIntegration extends Integration<AppsFlyerLib> {
 
     private static final String APPSFLYER_KEY = "AppsFlyer";
-
     final Logger logger;
     final AppsFlyerLib appsflyer;
     final String appsFlyerDevKey;
     final boolean isDebug;
     private Context context;
+    private String customerUserId;
 
-    private String customerUserId, currencyCode;
-
+    public static ConversionListenerDisplay cld;
     public static final Factory FACTORY = new Integration.Factory() {
         @Override
         public Integration<AppsFlyerLib> create(ValueMap settings, Analytics analytics) {
@@ -40,12 +39,14 @@ public class AppsflyerIntegration extends Integration<AppsFlyerLib> {
 
             String devKey = settings.getString("appsFlyerDevKey");
             boolean trackAttributionData = settings.getBoolean("trackAttributionData", false);
-            Context context = analytics.getApplication();
+            Application application = analytics.getApplication();
+            afLib.startTracking(application, devKey);
+            logger.verbose("AppsFlyer.getInstance().startTracking(%s, %s)", application, devKey.substring(0, 1) + "*****" + devKey.substring(devKey.length() - 2));
             if (trackAttributionData) {
                 AppsFlyerConversionListener listener = new ConversionListener(analytics);
-                afLib.registerConversionListener(context, listener);
+                afLib.registerConversionListener(application, listener);
             }
-            return new AppsflyerIntegration(context, logger, afLib, devKey);
+            return new AppsflyerIntegration(application, logger, afLib, devKey);
         }
 
         @Override
@@ -68,7 +69,7 @@ public class AppsflyerIntegration extends Integration<AppsFlyerLib> {
         super.onActivityCreated(activity, savedInstanceState);
         updateEndUserAttributes();
         appsflyer.startTracking(activity.getApplication(), appsFlyerDevKey);
-        logger.verbose("AppsFlyer.getInstance().startTracking(%s, %s)",activity.getApplication(), appsFlyerDevKey.substring(0,1)+"*****"+appsFlyerDevKey.substring(appsFlyerDevKey.length()-2) );
+        logger.verbose("AppsFlyer.getInstance().startTracking(%s, %s)", activity.getApplication(), appsFlyerDevKey.substring(0, 1) + "*****" + appsFlyerDevKey.substring(appsFlyerDevKey.length() - 2));
     }
 
     @Override
@@ -76,39 +77,38 @@ public class AppsflyerIntegration extends Integration<AppsFlyerLib> {
         return appsflyer;
     }
 
-
     @Override
     public void identify(IdentifyPayload identify) {
         super.identify(identify);
 
-        Traits traits = identify.traits();
         customerUserId = identify.userId();
 
-        if(appsflyer != null) {
+        if (appsflyer != null) {
             updateEndUserAttributes();
         } else {
             logger.verbose("couldn't update attributes");
         }
     }
 
-
     private void updateEndUserAttributes() {
 
-        appsflyer.setCurrencyCode(currencyCode);
-        logger.verbose("appsflyer.setCurrencyCode(%s)",currencyCode);
         appsflyer.setCustomerUserId(customerUserId);
-        logger.verbose("appsflyer.setCustomerUserId(%s)",customerUserId);
+        logger.verbose("appsflyer.setCustomerUserId(%s)", customerUserId);
         appsflyer.setDebugLog(isDebug);
-        logger.verbose("appsflyer.setDebugLog(%s)",isDebug);
+        logger.verbose("appsflyer.setDebugLog(%s)", isDebug);
     }
-
 
     @Override
     public void track(TrackPayload track) {
         String event = track.event();
         Properties properties = track.properties();
         appsflyer.trackEvent(context, event, properties);
-        logger.verbose("appsflyer.trackEvent(null, %s, %s)",context, event, properties);
+        logger.verbose("appsflyer.trackEvent(null, %s, %s)", context, event, properties);
+    }
+
+
+    public interface ConversionListenerDisplay {
+        void display(Map<String, String> attributionData);
     }
 
     static class ConversionListener implements AppsFlyerConversionListener {
@@ -118,34 +118,45 @@ public class AppsflyerIntegration extends Integration<AppsFlyerLib> {
             this.analytics = analytics;
         }
 
-        @Override public void onInstallConversionDataLoaded(Map<String, String> conversionData) {
+        @Override
+        public void onInstallConversionDataLoaded(Map<String, String> conversionData) {
             trackInstallAttributed(conversionData);
+            if (cld != null) {
+                cld.display(conversionData);
+            }
         }
 
-        @Override public void onInstallConversionFailure(String errorMessage) {
+        @Override
+        public void onInstallConversionFailure(String errorMessage) {
 
         }
 
-        @Override public void onAppOpenAttribution(Map<String, String> attributionData) {
+        @Override
+        public void onAppOpenAttribution(Map<String, String> attributionData) {
             trackInstallAttributed(attributionData);
+            if (cld != null) {
+                cld.display(attributionData);
+            }
         }
 
-        @Override public void onAttributionFailure(String errorMessage) {
+        @Override
+        public void onAttributionFailure(String errorMessage) {
 
         }
 
         void trackInstallAttributed(Map<String, String> attributionData) {
             // See https://segment.com/docs/spec/mobile/#install-attributed.
             Map<String, Object> campaign = new ValueMap() //
-                .putValue("source", attributionData.get("media_source"))
-                .putValue("name", attributionData.get("campaign"))
-                // .putValue("content", attributionData.get("?"))
-                // .putValue("adCreative", attributionData.get("?"))
-                .putValue("adGroup", attributionData.get("adgroup"));
+                    .putValue("source", attributionData.get("media_source"))
+                    .putValue("name", attributionData.get("campaign"))
+                    // .putValue("content", attributionData.get("?"))
+                    // .putValue("adCreative", attributionData.get("?"))
+                    .putValue("adGroup", attributionData.get("adgroup"));
+
 
             Properties properties = new Properties() //
-                .putValue("provider", "AppsFlyer")
-                .putValue("campaign", campaign);
+                    .putValue("provider", "AppsFlyer")
+                    .putValue("campaign", campaign);
             properties.putAll(attributionData);
 
             // Remove properties set in campaign.
--- segmenttestapp/src/main/java/com/appsflyer/segmenttestapp/MainActivity.java
+++ segmenttestapp/src/main/java/com/appsflyer/segmenttestapp/MainActivity.java
@@ -1,28 +1,96 @@
 package com.appsflyer.segmenttestapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Properties;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.android.integrations.appsflyer.AppsflyerIntegration;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AppsFlyer-Segment";
     private Analytics analytics;
+    private KeyValueAdapter adapter;
+    private EditText eventNameET;
+    private EditText keyET;
+    private EditText valueET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initListView();
+        ((Button) findViewById(R.id.button_add)).setOnClickListener(this);
+        eventNameET = (EditText) findViewById(R.id.event_name_editText);
+        keyET = (EditText) findViewById(R.id.key_text_editText);
+        valueET = (EditText) findViewById(R.id.value_text_editText);
+        valueET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    adapter.addItem(keyET.getText().toString(), valueET.getText().toString());
+                    keyET.requestFocus();
+//                    return true;
+                }
+                return false;
+            }
+        });
+
         Log.d(TAG, "AppsFlyer's Segment Integration TestApp is now initializing..");
-        analytics = new Analytics.Builder(this, "LTKg97K4uHOXI1udmMG9eGHsubnCCASQ")
+        analytics = new Analytics.Builder(this, "Your-Segment-Write-Key")
                 .use(AppsflyerIntegration.FACTORY).build();
+
+        AppsflyerIntegration.cld = new AppsflyerIntegration.ConversionListenerDisplay() {
+            @Override
+            public void display(Map<String, String> attributionData) {
+                for (String attrName : attributionData.keySet()) {
+                    Log.d(TAG, "attribute: " + attrName + " = " +
+                            attributionData.get(attrName));
+                }
+                //SCREEN VALUES//
+                StringBuilder sb = new StringBuilder();
+                sb.append("Install Type: ").append(attributionData.get("af_status")).append("\n");
+                sb.append("Media Source: ").append(attributionData.get("media_source")).append("\n");
+                sb.append("Click Time(GMT): ").append(attributionData.get("click_time")).append("\n");
+                sb.append("Install Time(GMT): ").append(attributionData.get("install_time")).append("\n");
+                final String conversionDataString = sb.toString();
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        TextView conversionTextView = (TextView) findViewById(R.id.conversionDataTextView);
+                        if (conversionTextView != null) {
+                            conversionTextView.setGravity(Gravity.CENTER_VERTICAL);
+                            conversionTextView.setText(conversionDataString);
+                        } else {
+                            analytics.logger("AF-SegmentTestApp").debug("could not load conversion data");
+                        }
+                    }
+                });
+
+            }
+        };
 //        Analytics.setSingletonInstance(analytics);
 
 //        ValueMap settings = new ValueMap().putValue("appsFlyerDevKey", "JkmJarFMos7svquk9gxQfC").putValue("trackAttributionData", true);
@@ -34,10 +102,105 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    private void initListView() {
+        adapter = new KeyValueAdapter();
+        ((ListView) findViewById(R.id.listView_items)).setAdapter(adapter);
+    }
+
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.track_button) {
-            analytics.track("Clicking button attribution!");
+            String eventName = eventNameET.getText().toString();
+            if (!eventName.equals("")) {
+                Properties properties = new Properties();
+                properties.putAll(adapter.mData);
+                analytics.track(eventName, properties);
+            } else {
+                analytics.track("Testing attribution!");
+            }
+        } else if (view.getId() == R.id.button_add) {
+            adapter.addItem(keyET.getText().toString(), valueET.getText().toString());
+        } else {
+
         }
     }
+
+    public void deleteClickHandler(View v) {
+        RelativeLayout vwParentRow = (RelativeLayout) v.getParent();
+        LinearLayout keyValContainer = (LinearLayout) vwParentRow.getChildAt(0);
+        String key = ((TextView) keyValContainer.getChildAt(0)).getText().toString();
+        adapter.mData.remove(key);
+        adapter.mKeys.remove(key);
+        adapter.notifyDataSetChanged();
+    }
+
+    public static class ViewHolder {
+        public TextView keyTextView;
+        public TextView valueTextView;
+        public Button deleteButton;
+    }
+
+    private class KeyValueAdapter extends BaseAdapter {
+
+        public Map<String, String> mData = new LinkedHashMap<>();
+        private List<String> mKeys;
+        private LayoutInflater mInflater;
+
+        public KeyValueAdapter(LinkedHashMap<String, String> data) {
+            mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mData = data;
+            mKeys = new ArrayList<>(mData.keySet());
+        }
+
+        public KeyValueAdapter() {
+            mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mKeys = new ArrayList<>(mData.keySet());
+        }
+
+        public void addItem(String key, String value) {
+            mData.put(key, value);
+            if (!mKeys.contains(key)) {
+                mKeys.add(key);
+            }
+            notifyDataSetChanged();
+            keyET.setText("");
+            valueET.setText("");
+        }
+
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mKeys.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            System.out.println("getView " + position + " " + convertView);
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.item1, null);
+                holder = new ViewHolder();
+                holder.keyTextView = (TextView) convertView.findViewById(R.id.key_text_item);
+                holder.valueTextView = (TextView) convertView.findViewById(R.id.value_text_item);
+                holder.deleteButton = (Button) convertView.findViewById(R.id.remove_button);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.keyTextView.setText(mKeys.get(position));
+            holder.valueTextView.setText(mData.get(mKeys.get(position)));
+            return convertView;
+        }
+
+    }
+
 }
