65d266e50a203d25aae9590f88ffbe5bf2a39771
Moris Gateno <moris.gateno@appsflyer.com>
2023-01-16 09:58:58 +0200

    saving changes of unit tests

--- app/src/test/java/com/segment/analytics/android/integrations/appsflyer/AppsflyerIntegrationTests.java
+++ app/src/test/java/com/segment/analytics/android/integrations/appsflyer/AppsflyerIntegrationTests.java
@@ -1,27 +1,35 @@
 package com.segment.analytics.android.integrations.appsflyer;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.ArgumentMatcher;
 import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.internal.platform.app.ActivityInvoker;
 
 import com.appsflyer.AFInAppEventParameterName;
 import com.appsflyer.AppsFlyerLib;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.ValueMap;
+import com.segment.analytics.integrations.Integration;
 import com.segment.analytics.integrations.Logger;
 import static  org.mockito.Mockito.*;
-
 import java.lang.reflect.Field;
-
+import java.lang.reflect.Method;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+
 @RunWith(AndroidJUnit4.class)
 public class AppsflyerIntegrationTests {
     @Test
@@ -93,10 +101,76 @@ public class AppsflyerIntegrationTests {
         verify(appsFlyerLib,never()).start(any());
     }
 
+    @Test
+    public void testAppsflyerIntegration_FACTORYCreate_happyFlow() throws Exception {
+        MockedStatic<AppsFlyerLib> staticAppsFlyerLib = mockStatic(AppsFlyerLib.class);
+        AppsFlyerLib appsFlyerLib = mock(AppsFlyerLib.class);
+        staticAppsFlyerLib.when(AppsFlyerLib::getInstance).thenReturn(appsFlyerLib);
+        Analytics analytics = mock(Analytics.class);
+        ValueMap settings = new ValueMap();
+        settings.put("appsFlyerDevKey" , "devKey");
+        settings.put("trackAttributionData" , true);
+        Logger logger = new Logger("test", Analytics.LogLevel.INFO);
+        Mockito.when(analytics.logger("AppsFlyer")).thenReturn(logger);
+        Application app = mock(Application.class);
+        Mockito.when(analytics.getApplication()).thenReturn(app);
+        AppsflyerIntegration.deepLinkListener = mock(AppsflyerIntegration.ExternalDeepLinkListener.class);
+
+        Integration<AppsFlyerLib> integration= (Integration<AppsFlyerLib>) AppsflyerIntegration.FACTORY.create(settings,analytics);
+        verify(appsFlyerLib).setDebugLog(logger.logLevel!=Analytics.LogLevel.NONE);
+        ArgumentCaptor<AppsflyerIntegration.ConversionListener> captorListener = ArgumentCaptor.forClass(AppsflyerIntegration.ConversionListener.class);
+        ArgumentCaptor<String> captorDevKey = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Context> captorContext = ArgumentCaptor.forClass(Context.class);
+        verify(appsFlyerLib).init(captorDevKey.capture(), captorListener.capture() , captorContext.capture());
+        Assert.assertTrue(captorListener.getValue()!=null);
+        Assert.assertTrue(captorListener.getValue() instanceof AppsflyerIntegration.ConversionListener);
+        Assert.assertTrue(captorDevKey.getValue() == settings.getString("appsFlyerDevKey"));
+        Assert.assertTrue(captorContext.getValue() == app.getApplicationContext());
+        verify(appsFlyerLib).subscribeForDeepLink(AppsflyerIntegration.deepLinkListener);
+    }
 
 //    @Test
-//    public void testAppsflyerIntegration_getUnderlyingInstance_happyFlow() throws Exception {
-//        Assert.assertTrue(AppsflyerIntegration);
-//    }
+//    public void testAppsflyerIntegration_FACTORYCreate_nilFlow() throws Exception {
+//        MockedStatic<AppsFlyerLib> staticAppsFlyerLib = mockStatic(AppsFlyerLib.class);
+//        AppsFlyerLib appsFlyerLib = mock(AppsFlyerLib.class);
+//        staticAppsFlyerLib.when(AppsFlyerLib::getInstance).thenReturn(appsFlyerLib);
+//        Analytics analytics = mock(Analytics.class);
+//        ValueMap settings = new ValueMap();
+//        settings.put("appsFlyerDevKey_wrong" , "devKey");
+//        settings.put("trackAttributionData_wrong" , true);
+//        Logger logger = new Logger("test", Analytics.LogLevel.INFO);
+//        Mockito.when(analytics.logger("AppsFlyer")).thenReturn(logger);
+//        Application app = mock(Application.class);
+//        Mockito.when(analytics.getApplication()).thenReturn(app);
+//        AppsflyerIntegration.deepLinkListener = mock(AppsflyerIntegration.ExternalDeepLinkListener.class);
+//
+//        Integration<AppsFlyerLib> integration =
+//                (Integration<AppsFlyerLib>) AppsflyerIntegration.FACTORY.create(settings,analytics);
+//        verify(appsFlyerLib).setDebugLog(logger.logLevel!=Analytics.LogLevel.NONE);
+//        ArgumentCaptor<AppsflyerIntegration.ConversionListener> captorListener = ArgumentCaptor.forClass(AppsflyerIntegration.ConversionListener.class);
+//        ArgumentCaptor<String> captorDevKey = ArgumentCaptor.forClass(String.class);
+//        ArgumentCaptor<Context> captorContext = ArgumentCaptor.forClass(Context.class);
+//        verify(appsFlyerLib).init(captorDevKey.capture(), captorListener.capture() , captorContext.capture());
+//        Assert.assertTrue(captorListener.getValue()!=null);
+//        Assert.assertTrue(captorListener.getValue() instanceof AppsflyerIntegration.ConversionListener);
+//        Assert.assertTrue(captorDevKey.getValue() == settings.getString("appsFlyerDevKey"));
+//        Assert.assertTrue(captorContext.getValue() == app.getApplicationContext());
+//        verify(appsFlyerLib).subscribeForDeepLink(AppsflyerIntegration.deepLinkListener);
+
+    @Test
+    public void testAppsflyerIntegration_FACTORYKEY_happyFlow() throws Exception {
+            Assert.assertTrue(AppsflyerIntegration.FACTORY.key().equals("AppsFlyer"));
+    }
+
+    @Test
+    public void testAppsflyerIntegration_onActivityCreated_nilFlow() throws Exception {
+        MockedStatic<AppsFlyerLib> staticAppsFlyerLib = mockStatic(AppsFlyerLib.class);
+        AppsFlyerLib appsFlyerLib = mock(AppsFlyerLib.class);
+        staticAppsFlyerLib.when(AppsFlyerLib::getInstance).thenReturn(appsFlyerLib);
+        AppsflyerIntegration.manualMode=false;
+        AppsflyerIntegration appsflyerIntegration = mock(AppsflyerIntegration.class);
+        appsflyerIntegration.onActivityCreated(mock(Activity.class), mock(Bundle.class));
+        verify(appsFlyerLib).start(any());
+    }
 
 }
