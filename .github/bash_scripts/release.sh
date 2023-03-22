e8e9653b839be4a5f83c9ab82373285eac39a254
Moris Gateno <moris.gateno@appsflyer.com>
2023-03-22 15:32:04 +0200

    added release steps(yml files)

--- .github/bash_scripts/release.sh
+++ .github/bash_scripts/release.sh
@@ -2,14 +2,9 @@
 
 releaseversion=$1
 
-sed -r -i '' "s/version_release = \'[0-9]+\.[0-9]+\.[0-9]+\'/version_release = \'$releaseversion\'/g" segment-appsflyer-ios.podspec
-
-sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*)/\1,\'$releaseversion\'/g" examples/SwiftPodsSample/Podfile
-sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*)/\1,\'$releaseversion\'/g" examples/ObjcPodsSample/Podfile
-
 sed -i '' 's/^/* /' "releasenotes.$releaseversion"
 NEW_VERSION_RELEASE_NOTES=$(cat "releasenotes.$releaseversion")
-NEW_VERSION_SECTION="### $releaseversion\n* Updated iOS SDK to v$releaseversion\n$NEW_VERSION_RELEASE_NOTES\n\n"
+NEW_VERSION_SECTION="### $releaseversion\n$NEW_VERSION_RELEASE_NOTES\n\n"
 echo -e "$NEW_VERSION_SECTION$(cat RELEASENOTES.md)" > RELEASENOTES.md
 
 rm -r "releasenotes.$releaseversion"
