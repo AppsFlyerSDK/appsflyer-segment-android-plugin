96c9dcaa6616afd6a6d3abe4a8cde557acb352ef
Moris Gateno <moris.gateno@appsflyer.com>
2023-04-30 15:24:47 +0300

    releaseing to maven QA

--- .github/bash_scripts/pre_release.sh
+++ .github/bash_scripts/pre_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 appsflyerversion=$1
+rc=$2
 
 sed -E -i '' "s/(.*af-android-sdk:)([0-9]+\.[0-9]+\.[0-9]+)'/\1$appsflyerversion\'/g" app/build.gradle
 
@@ -10,7 +11,9 @@ version_code=$(grep -E 'VERSION_CODE=([0-9]+)' gradle.properties | grep -o '[0-9
 version_code=$((version_code+1)) 
 sed -E -i '' "s/VERSION_CODE=([0-9]+)/VERSION_CODE=$version_code/g" gradle.properties
 
-sed -E -i '' "s/VERSION_NAME=([0-9]+\.[0-9]+\.[0-9]+)/VERSION_NAME=$appsflyerversion/g" gradle.properties
+sed -E -i '' "s/VERSION_NAME=([0-9]+\.[0-9]+\.[0-9]+)/VERSION_NAME=$appsflyerversion-rc$rc/g" gradle.properties
+
+sed -E -i '' "s/(POM_ARTIFACT_ID=.*)/\1-beta/g" gradle.properties
 
 sed -E -i '' "s/(Built with AppsFlyer Android SDK.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerversion\3/g" Readme.md
 sed -E -i '' "s/(.*appsflyer:segment-android-integration:)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerversion\3/g" Readme.md
