96cabd57fbabd1db79484b7f784926267aa6a58f
Moris Gateno <moris.gateno@appsflyer.com>
2023-04-30 17:47:14 +0300

    yaml chnages

--- .github/bash_scripts/release.sh
+++ .github/bash_scripts/release.sh
@@ -8,3 +8,7 @@ NEW_VERSION_SECTION="### $releaseversion\n$NEW_VERSION_RELEASE_NOTES\n\n"
 echo -e "$NEW_VERSION_SECTION$(cat RELEASENOTES.md)" > RELEASENOTES.md
 
 rm -r "releasenotes.$releaseversion"
+
+sed -E -i '' "s/VERSION_NAME=([0-9]+\.[0-9]+\.[0-9]+).*/VERSION_NAME=$appsflyerversion/g" gradle.properties
+
+sed -E -i '' "s/(POM_ARTIFACT_ID=.*)-beta/\1/g" gradle.properties
