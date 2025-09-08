504b960cd0d2c0fea3ad858616e2a3ac1958480f
andriidudka <andrii.dudka@appsflyer.com>
2025-09-08 15:17:16 +0300

    Update RELEASENOTES.md

--- .github/bash_scripts/release.sh
+++ .github/bash_scripts/release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#releaseversion=$1
-#
+releaseversion=$1
+
 #sed -i '' 's/^/* /' "releasenotes.$releaseversion"
 #NEW_VERSION_RELEASE_NOTES=$(cat "releasenotes.$releaseversion")
 #NEW_VERSION_SECTION="### $releaseversion\n$NEW_VERSION_RELEASE_NOTES\n\n"
 #echo -e "$NEW_VERSION_SECTION$(cat RELEASENOTES.md)" > RELEASENOTES.md
-#
+
 #rm -r "releasenotes.$releaseversion"
 
 sed -E -i '' "s/VERSION_NAME=([0-9]+\.[0-9]+\.[0-9]+).*/VERSION_NAME=$releaseversion/g" gradle.properties
